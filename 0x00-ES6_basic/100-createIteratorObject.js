95784a249b440a433754816a5bf6e311ae86258f
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-26 23:57:50 +0100

    advanced task 100

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees.push(...item);
+  }
+
+  return allEmployees;
+}
