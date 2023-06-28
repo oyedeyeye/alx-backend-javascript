79789a3de51da5df3cf4c658dcae4fc993830a23
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-28 14:36:45 +0100

    task 6 new update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.map((result) => ({
+      const resArr = results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
       }));
+      return resArr;
     })
     .catch(() => new Error());
 }
