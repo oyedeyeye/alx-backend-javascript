e530630aa41a138559027090e5599e1690f9656d
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-28 14:19:00 +0100

    task 6 updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,5 +8,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
       }));
-    });
+    })
+    .catch(() => new Error());
 }
