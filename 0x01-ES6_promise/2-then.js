c59c3df6dcfcc707eb6b8b363c872054746a599d
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-28 10:46:14 +0100

    task 2 updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const body = { status: 200, body: 'success' };
-
   return promise
-    .then(() => body)
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
