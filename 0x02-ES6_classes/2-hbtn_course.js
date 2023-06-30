622aa81add80f9a17f4e06842ea9749c82370a77
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-30 16:22:49 +0100

    task 2 updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -32,10 +32,10 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!(Array.isArray(value))) {
+    if (!(value instanceof Array) {
       throw new TypeError('Students must be an array of strings');
     }
-    if (!value.every((student) => student === 'string')) {
+    else if (!(value.every((student) => student === 'string'))) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
