e7bf6ca0814357be3a9b9f687d0f5dba9678cdc8
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-05 12:21:29 +0100

    advanced task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endPoint) {
+  if (!(weakMap.has(endPoint))) {
+    weakMap.set(endPoint, 0);
+  }
+  weakMap.set(endPoint, weakMap.get(endPoint) + 1);
+  if (weakMap.get(endPoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
