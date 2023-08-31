4adcccb1d00201c8d9ccecd18b87a3d4c3301432
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-31 14:24:46 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,3 +1,32 @@
-/**
- * countStudents: function that reads the database file synchronously
- */
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+  // Synchronous method blocks other paralell processes
+
+  const data = fs.readFileSync(path, 'utf-8');
+  const students = data.split('\n')
+    .map((student) => student.split(','))
+    .filter(((student) => student.length === 4 && student[0] !== 'firstname'))
+    .map((student) => ({
+      firstname: student[0],
+      lastname: student[1],
+      age: student[2],
+      field: student[3],
+    }));
+
+  const csStudents = students
+    .filter((student) => student.field === 'CS')
+    .map((student) => student.firstname);
+  const sweStudents = students
+    .filter((student) => student.field === 'SWE')
+    .map((student) => student.firstname);
+
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+  console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+}
+
+module.exports = countStudents;
