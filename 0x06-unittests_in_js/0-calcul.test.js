375ff7b4443092058bac1f2b802a507149addc84
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-04 15:51:07 +0100

    Basic test with Mocha Assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,30 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber", function() {
+describe("CalculateNumber", function() {
   it("first number rounded", function() {
     assert.equal(calculateNumber(1.5, 3), 5);
+    assert.equal(calculateNumber(1.4, 0), 1);
+    assert.notEqual(calculateNumber(1.2, 1), 3);
   });
-});
 
-describe("calculateNumber", function() {
   it("Second number rounded", function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1, 0.75), 2);
+    assert.notEqual(calculateNumber(0.45, 2), 3);
   });
-});
 
-describe("calculateNumber", function() {
   it("Both number rounded", function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(7.9, 3.2), 11);
+    assert.equal(calculateNumber(1.8, 0.7), 3);
+    assert.notEqual(calculateNumber(0.45, 2.55), 5);
   });
-});
 
-describe("calculateNumber", function() {
   it("Sum whole numbers", function() {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(0, 0), 0);
+    assert.equal(calculateNumber(1, 37), 38);
+    assert.equal(calculateNumber(6, 75), 81);
+    assert.notEqual(calculateNumber(0, 2), 3);
   });
 });
