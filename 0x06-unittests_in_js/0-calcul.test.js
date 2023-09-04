658f2647df48afd43acd19b3ccb89f86628ab07a
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-04 15:37:27 +0100

    Basic test with Mocha Assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,25 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe("calculateNumber", function() {
-  it("Both number rounded", function() {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it("Sum whole numbers", function() {
-    assert.equal(calculateNumber(1, 3), 4);
-  });
-
   it("first number rounded", function() {
     assert.equal(calculateNumber(1.5, 3), 5);
   });
+});
 
+describe("calculateNumber", function() {
   it("Second number rounded", function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  });
+});
+
+describe("calculateNumber", function() {
+  it("Both number rounded", function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
+
+describe("calculateNumber", function() {
+  it("Sum whole numbers", function() {
+    assert.equal(calculateNumber(1, 3), 4);
   });
 });
