49302a6b93a0bf99f3e854511acb40e23e2e7322
Hezekiah Oyemade <oyedey@gmail.com>
2023-09-05 16:53:35 +0100

    Basic Integration Testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,17 +2,26 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Integration Testing', () => {
-  describe('GET request method', () => {
-    it('Status code and response body', () => {
-      const params = {
-        url: 'http://localhost:7865',
-        method: 'GET'
-      };
 
+    const params = {
+      url: 'http://localhost:7865'
+    };
+
+  describe('GET request method', () => {
+
+    it('should verify the request method', () => {
       request(params, (err, res, body) => {
         // console.error(err); // print the error should in case one occurs
         expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('Status code and response body', () => {
+      request(params, (err, res, body) => {
+        // console.error(err); // print the error should in case one occurs
         expect(body).to.equal('Welcome to the payment system');
+        done();
       });
     });
   });
